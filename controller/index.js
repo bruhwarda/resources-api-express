74da2b2fb93c099665192d6e3edea17b03b9a5d9
warda sheraz <wbywardah@gmail.com>
2023-10-07 23:18:22 +0500

    controller index route

---
+++ controller/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //basically exporting all the elements used
+  resourceController: require("./resources/resourcesController"),
+  //require work as import that we use in react
+};
